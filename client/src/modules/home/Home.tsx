449d158a29c118912c37ee7903d21c60f4be8b35
Jan <etomanon@gmail.com>
2019-09-30 19:26:05 +0200

    convert currency

--- client/src/modules/home/Home.tsx
+++ client/src/modules/home/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Flex } from "@rebass/grid";
+import Decimal from "decimal.js-light";
 
 import { selectorData } from "../../redux/data/selectors";
 import { dataGet } from "../../redux/data/actions";
@@ -10,12 +11,25 @@ import { HomeForm } from "./HomeForm";
 import { FormValues } from "./_formik";
 
 export const Home: React.FC = () => {
+  // final converted value
+  const [converted, setConverted] = useState<string | null>(null);
+  // formik value after submit
   const [formData, setFormData] = useState<FormValues | null>(null);
   const data = useSelector(selectorData);
   const dispatch = useDispatch();
+  // load exchange rates
   useEffect(() => {
     dispatch(dataGet());
   }, [dispatch]);
+  // update converted value if formData changes
+  useEffect(() => {
+    if (formData && formData.amount && formData.rate) {
+      const currencyOriginal = new Decimal(formData.amount);
+      const currencyTarget = new Decimal(formData.rate.value);
+      const converted = currencyOriginal.dividedBy(currencyTarget).toFixed(2);
+      setConverted(converted);
+    }
+  }, [formData]);
   const onSubmit = (values: FormValues) => setFormData(values);
   return (
     <>
@@ -29,9 +43,7 @@ export const Home: React.FC = () => {
         <Header>Currency Converter</Header>
         <HomeForm onSubmit={onSubmit} rates={data.rates} />
         {formData && formData.amount && formData.rate && (
-          <SubHeader>{`${formData.amount} CZK = ${(
-            formData.amount / formData.rate.value
-          ).toFixed(2)} ${formData.rate.label}`}</SubHeader>
+          <SubHeader>{`${formData.amount} CZK = ${converted} ${formData.rate.label}`}</SubHeader>
         )}
       </Flex>
     </>
